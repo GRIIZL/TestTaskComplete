9764160041c727e7da979e07e9dcc16bacd67557
GRIIZL <72096275+GRIIZL@users.noreply.github.com>
2024-05-18 18:36:48 +0300

    Add project files.

---
+++ TestTask/Services/Implementations/OrderService.cs
@@ -0,0 +1,31 @@
+﻿using TestTask.Data;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using TestTask.Enums;
+
+namespace TestTask.Services.Implementations
+{
+    public class OrderService : IOrderService
+    {
+
+        private readonly ApplicationDbContext _dbContext;
+
+        public OrderService(ApplicationDbContext dbContext) 
+        {
+            _dbContext = dbContext;
+        }
+
+        public Task<Order> GetOrder()
+        {
+           return _dbContext.Orders.Where(order => order.Quantity > 1).OrderByDescending(order => order.CreatedAt).FirstAsync();
+        }
+
+        public Task<List<Order>> GetOrders()
+        {
+            return _dbContext.Orders.Where(order => order.User.Status == UserStatus.Active).OrderBy(order => order.CreatedAt).ToListAsync();
+            //return _dbContext.Orders.Join(_dbContext.Users, order => order.UserId, user => user.Id, (order, user) => new {order, user}).Where(x => x.user.Status == UserStatus.Active).Select(x=>x.order).OrderBy(order =>order.CreatedAt).ToListAsync();
+        }
+    }
+}
---
+++ TestTask/Services/Implementations/UserService.cs
@@ -0,0 +1,36 @@
+﻿using TestTask.Data;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+
+namespace TestTask.Services.Implementations
+{
+    public class UserService : IUserService
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public UserService(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public Task<User> GetUser()
+        {
+            //return _dbContext.Users.Where(user => user.Orders.Any(order => order.CreatedAt.Year == 2003)).Where(user => user.Orders.MaxBy(order => order.Quantity * order.Price)).FirstAsync();
+            return _dbContext.Users.Select(user => new
+            {
+                User = user,
+                MaxOrderValue = user.Orders.Where(order => order.CreatedAt.Year == 2003).Max(order => order.Quantity * order.Price)
+            })
+                               .OrderByDescending(u => u.MaxOrderValue)
+                               .Select(u => u.User)
+                               .FirstAsync();
+        }
+
+        public Task<List<User>> GetUsers()
+        {
+            return _dbContext.Users.Where(user => user.Orders.Any(order => order.CreatedAt.Year == 2010 && order.Status == Enums.OrderStatus.Paid)).ToListAsync();
+        }
+    }
+}
